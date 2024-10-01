49a9b32e74667795aaa1fea7c3249f3b80dbfb91
KhalidEl-MariAa <khalidmaria13@gmail.com>
2024-10-01 09:42:13 +0300

    - create textstyles file for textsyles used in app. - add loginscreen and homescreen textstyles.

---
+++ lib/core/textstyles/textstyles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zaghrota_app/core/textstyles/weights.dart';
+
+
+class Textstyles {
+  static  TextStyle hintTextStyle =  TextStyle(color: Colors.black,fontSize: 15.sp,);
+  static  TextStyle hintTextStyle2 =  TextStyle(color: Colors.grey,fontSize: 15.sp);
+  static TextStyle weddingNames = TextStyle(color: Colors.black,fontSize: 15.sp,fontWeight: FontWeights.medium);
+  static TextStyle listViewTitles = TextStyle(fontWeight: FontWeights.thin,fontSize: 20.sp);
+  static TextStyle heartViewTextStyle = TextStyle(fontSize: 13.sp);
+}
