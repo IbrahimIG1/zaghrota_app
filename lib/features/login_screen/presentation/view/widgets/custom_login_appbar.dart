7805d5f3cfe40d266b669ec8696efc7e19f69afc
KhalidEl-MariAa <khalidmaria13@gmail.com>
2024-10-01 10:01:53 +0300

    - Add loginscreen appbar and refactor it in custom_login_appbar file.
    - Add main login screen Image and refactor it in image_view file.
    
    - Add text inputs textfields and refactor it in cutsom_text_field file.
    
    - Add date input field and refactor it in custom_datefield file.
    
    - Add button for sign in and refactor it in sign_in_button file.
    
    - Refactor textfields, datefield and signin button in form_textfields file.

---
+++ lib/features/login_screen/presentation/view/widgets/custom_login_appbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:zaghrota_app/core/colors/colors.dart';
+
+class CustomLoginAppbar extends StatelessWidget {
+  const CustomLoginAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+                    height: 42,
+                    decoration: const BoxDecoration(
+          // color: Colors.red,
+          gradient: AppColors.appBarLinearColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+            )
+          
+          )
+                    );
+  }
+}
