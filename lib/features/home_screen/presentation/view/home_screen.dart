c9caace5b0e985f3ba637fe38c3b537db4beabdb
KhalidEl-MariAa <khalidmaria13@gmail.com>
2024-10-05 14:06:16 +0300

    - Remove unused import in home_screen file

--- lib/features/home_screen/presentation/view/home_screen.dart
+++ lib/features/home_screen/presentation/view/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:zaghrota_app/core/colors/colors.dart';
-import 'package:zaghrota_app/core/usable/sizedbox.dart';
 import 'package:zaghrota_app/features/home_screen/presentation/view/widgets/heart_drawer.dart';
 import 'package:zaghrota_app/features/home_screen/presentation/view/widgets/homechoices_listview.dart';
 import 'package:zaghrota_app/features/login_screen/presentation/view/widgets/custom_login_appbar.dart';
