3cd671db91e9728c0f8ff54eeb504b44881964a5
KhalidEl-MariAa <khalidmaria13@gmail.com>
2024-10-05 14:07:45 +0300

    - Refactor the data structure.

--- lib/features/home_screen/presentation/view/widgets/homechoices_listview.dart
+++ lib/features/home_screen/presentation/view/widgets/homechoices_listview.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zaghrota_app/core/colors/colors.dart';
+import 'package:zaghrota_app/core/navigation/screen_names.dart';
 import 'package:zaghrota_app/core/textstyles/textstyles.dart';
 
 class HomechoicesListview extends StatelessWidget {
@@ -7,16 +9,35 @@ class HomechoicesListview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map<String,List> listviewData  = {
-      "titles":["قرأة الفاتحة","الشبكة","تجهيزات الفرح","الحنة","الفرح","شهر العسل"],
-      "images":["assets/images/homepage_images/man's hand holding woman's hand.png",
-      "assets/images/homepage_images/Wedding rings.png",
-      "assets/images/homepage_images/wedding-couple-flat.png",
-      "assets/images/homepage_images/Young joyful women.png",
-      "assets/images/homepage_images/music festival.png",
-      "assets/images/homepage_images/St Valentine Day Couple.png"
-      ],
-     };
+    List listviewData= [
+      {
+      "title":"قرأة الفاتحة",
+      "image":"assets/images/homepage_images/man's hand holding woman's hand.png",
+      "navigation":ScreenNames.weddingItemsScreen
+      },
+      {
+      "title":"الشبكة",
+      "image":"assets/images/homepage_images/Wedding rings.png",
+      },
+      {
+      "title":"تجهيزات الفرح",
+      "image":"assets/images/homepage_images/wedding-couple-flat.png",
+      },
+      {
+        "title":"الحنة",
+      "image":"assets/images/homepage_images/Young joyful women.png",
+      },
+      {
+      "title":"الفرح",
+      "image":"assets/images/homepage_images/music festival.png",
+      },
+      {
+        "title":"شهر العسل",
+      "image":"assets/images/homepage_images/St Valentine Day Couple.png",
+
+      }
+    ];
+     final args = ModalRoute.of(context)?.settings.arguments as DateTime;
     return  SizedBox(
       height:0.56.sh ,
       child: ListView.builder(
@@ -25,38 +46,43 @@ class HomechoicesListview extends StatelessWidget {
                 
                 mainAxisAlignment: index%2==0? MainAxisAlignment.start:MainAxisAlignment.end,
                children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 250.w,
-                      child:index%2==0?Image.asset("assets/images/polygons/Polygon 62.png",fit: BoxFit.fitWidth,):
-                      Image.asset("assets/images/polygons/Polygon 60.png",fit: BoxFit.fitWidth,)
-                      ),
-                    Padding(
-                      padding:  EdgeInsets.symmetric(vertical: 40.sp,
-                      horizontal:index%2==0?30.sp:40.sp),
-                      child: Center(
-                        child: CircleAvatar(
-                          radius: 90.sp,
-                          backgroundColor: Colors.white,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: 100.sp,
-                                child: Image.asset(listviewData["images"]![index],
-                                fit: BoxFit.fitWidth,)),
-                              Text(listviewData["titles"]![index],style: Textstyles.listViewTitles,)
-                            ],
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, listviewData[index]["navigation"],arguments: args);
+                  },
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        width: 250.w,
+                        child:index%2==0?Image.asset("assets/images/polygons/Polygon 62.png",fit: BoxFit.fitWidth,):
+                        Image.asset("assets/images/polygons/Polygon 60.png",fit: BoxFit.fitWidth,)
+                        ),
+                      Padding(
+                        padding:  EdgeInsets.symmetric(vertical: 42.r,
+                        horizontal:index%2==0?30.sp:40.sp),
+                        child: Center(
+                          child: CircleAvatar(
+                            radius: 90.r,
+                            backgroundColor: AppColors.scaffoldColor,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 100.sp,
+                                  child: Image.asset(listviewData[index]["image"],
+                                  fit: BoxFit.fitWidth,)),
+                                Text(listviewData[index]["title"],style: Textstyles.listViewTitles,)
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 )
                ],
               ),
-              itemCount: listviewData["titles"]!.length,
+              itemCount: listviewData.length,
               shrinkWrap: true,),
     );
   }
